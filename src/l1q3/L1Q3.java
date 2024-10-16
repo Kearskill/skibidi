e9f53f45571decbb7df8aead87b63ae4e96eb983
Kearskill <kearskill@gmail.com>
2024-10-17 01:22:22 +0800

    skibidi dup dup yesy yesy eysey

---
+++ src/l1q3/L1Q3.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package l1q3;
+
+/**
+ *
+ * @author User
+ */
+public class L1Q3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("yo how do i do this?");
+    }
+    
+}
